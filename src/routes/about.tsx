12ef63d88e6f18d06895dbcaf94e4a36089e8b35
Denzell Griffith <enrique213f@gmail.com>
2024-09-13 11:21:05 -0600

    prueba de clases de tailwind

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -8,7 +8,7 @@ export const Route = createFileRoute('/about')({
 function AboutComponent() {
   return (
     <div className="p-2">
-      <h3>About</h3>
+      <h3 className='text-3xl font-bold underline'>About</h3>
     </div>
   )
 }
